f1d1a03d402db3202a64586783a666dcd5b00c3b
uppkaram <uppkaram@sheridancollage.ca>
2023-05-28 20:28:27 -0400

    Branch

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/* Modifier-Karamjeet Uppal  Student Id - 991727467 */
 package card;
 
 /**
